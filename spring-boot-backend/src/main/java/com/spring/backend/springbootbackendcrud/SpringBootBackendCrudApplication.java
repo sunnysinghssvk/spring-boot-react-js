b6cbc3ee423f9389a9a5c3a60bb3d9376525c7f1
Sunny <sunnysinghssvk@gmail.com>
2024-03-16 18:02:03 +0530

    Initial commit

---
+++ spring-boot-backend/src/main/java/com/spring/backend/springbootbackendcrud/SpringBootBackendCrudApplication.java
@@ -0,0 +1,13 @@
+package com.spring.backend.springbootbackendcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendCrudApplication.class, args);
+	}
+
+}
